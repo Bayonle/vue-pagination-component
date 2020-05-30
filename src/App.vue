831b8b2213583a34f1643d9b4583389d30b2f67e
Bayonle Amzat <zedcrest@Zedcrests-MacBook-Pro.local>
2020-05-30 13:09:01 +0100

    📦 NEW: Pagination component setup with static data

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <pagination />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Pagination from './components/Pagination.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Pagination
   }
 }
 </script>
